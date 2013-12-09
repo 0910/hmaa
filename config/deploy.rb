8ebeab8b30df0ffc7e16440afc5490ccebf14d83
Octavio <octanks@hotmail.com>
2013-12-09 16:23:49 -0300

    config git repository

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:nubis/jacinta.git"
+set :repository, "git@github.com:0910/hmaa.git"
 set :branch, "master"
 set :keep_releases, 2
 
